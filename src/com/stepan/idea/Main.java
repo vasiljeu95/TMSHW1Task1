dd2f4f90789c916a9ae842554c6270076a380ead
vasiljeu95 <vasilyeu95@gmail.com>
2021-11-29 00:37:33 +0300

    secondHWcommit

--- src/com/stepan/idea/Main.java
+++ src/com/stepan/idea/Main.java
@@ -7,15 +7,25 @@ public class Main {
         double num = sc.nextDouble();
 
         if (num != 0) {
-            if (num < 10) {
+            if (Math.abs(num) < 10) {
                 System.out.println("this is a single digit number");
-            } else if (num < 100) {
+            } else if (Math.abs(num) < 100) {
                 System.out.println("this is a two-digit number");
-            } else if (num < 1000) {
+            } else if (Math.abs(num) < 1000) {
                 System.out.println("this is a three-digit number");
             }
+
+            if (num < 0) {
+                System.out.println("this is a negative number");
+            } else if (num > 0) {
+                System.out.println("this is a positive number");
+            }
+
+            String number = Double.toString(Math.abs(num));
+            int n = number.length();
+            System.out.println(n-2);
         } else {
-            System.out.println("ERROR");
+            System.out.println("ERROR, number = 0 !!!");
         }
     }
 }
